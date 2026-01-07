c18bcdd8c870a52608b7d9d25f64b5f45809125c
Lolo952 <souirileila@gmail.com>
2026-01-07 16:39:53 +0100

    Supprime app.js (inutile avec Expo Router)

--- app.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>CosmetiShop App.js 🚀</Text>
-    </View>
-  );
-}
